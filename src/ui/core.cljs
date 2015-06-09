d2c946ce198dc3972bb3ad472dbc7e3cf8ee01ce
Joseph Osborn <jcosborn@ucsc.edu>
2015-06-09 13:57:18 -0700

    Draw ticks at right locations. Jump around with shift-left and shift-right. Put scroll-x into app-state.

--- src/ui/core.cljs
+++ src/ui/core.cljs
@@ -84,44 +84,72 @@
       (-update-async-image data owner))
     om/IWillReceiveProps
     (will-receive-props [_ new-props]
-      (println "RECV make query for " (:now new-props))
-      (when-not (= new-props (om/get-props owner))
+      (when (or (not= (:now new-props) (:now (om/get-props owner)))
+                (not= (:source new-props) (:source (om/get-props owner))))
+        (println "RECV make query for" (:now new-props) "vs" (:now (om/get-props owner)))
         (-update-async-image new-props owner)))
     om/IRenderState
     (render-state [_ {image-data :image-data}]
       (dom/img (clj->js {:src   image-data :width 640 :height 480
                          :style {:margin-left 80}})))))
 
-(defn frame-offset-x [frame scroll-width context]
-  (let [frames-per-pixel (/ scroll-width context)]
-    (* frames-per-pixel frame)))
-
 (defn floor [a] (.floor js/Math a))
 
 (defn clip [lo x hi]
   (min hi (max lo x)))
 
+(defn frame-offset-x [frame scroll-width context]
+  (let [frames-per-pixel (/ scroll-width context)]
+    (floor (+ 2 (* frames-per-pixel frame)))))
+
 (defn inv-frame-offset-x [x scroll-width context duration]
   (let [frames-per-pixel (/ scroll-width context)]
-    (clip 0 (floor (/ x frames-per-pixel)) duration)))
+    (clip 0 (floor (/ x frames-per-pixel)) (dec duration))))
+
+(defn shown-tick-frames [w context left-x duration]
+  (let [frame-skip 32
+        left-frame (inv-frame-offset-x left-x w context duration)
+        left-frame (- left-frame (mod left-frame frame-skip))
+        right-frame (+ left-frame context frame-skip)
+        last-frame (dec duration)
+        frames-maybe-missing-end (range left-frame (min right-frame last-frame) frame-skip)
+        frames (if (< right-frame last-frame)
+                 frames-maybe-missing-end
+                 (concat frames-maybe-missing-end [last-frame]))]
+    frames))
 
 (defn jump-to-frame! [frame]
   (println "jump to " frame)
-  (swap! app-state update-in [:now] (fn [_] frame)))
+  (swap! app-state update-in [:now] (fn [_] frame))
+  ;jump scroll-x
+  )
 
 ; Do this goofy declare/remove/define/add dance to make sure we don't put two
 ; event handlers on the document.
 (declare handle-keyboard!)
 (.removeEventListener js/document "keydown" handle-keyboard!)
 (defn handle-keyboard! [e]
-  (println "K:" (.-keyCode e))
-  (case (.-keyCode e)
-    38 true                                                 ; up
-    40 true                                                 ; down
-    37 (jump-to-frame! (dec (:now @app-state)))             ; left
-    39 (jump-to-frame! (inc (:now @app-state)))             ; right
-    true)
-  (.preventDefault e))
+  (println "K:" (.-keyCode e) "shift:" (.-shiftKey e))
+  (let [now (:now @app-state)
+        duration (:duration @app-state)
+        left-x (get-in @app-state [:timeline :scroll-x])
+        scroll-width (:scroll-width @app-state)
+        shown-ticks (shown-tick-frames scroll-width (:context (:timeline @app-state)) left-x duration)
+        nearest-tick-left (or (last (filter #(< % now) shown-ticks)) 0)
+        nearest-tick-right (or (first (filter #(> % now) shown-ticks)) (dec duration))]
+    (case (.-keyCode e)
+      38 true                                               ; up
+      40 true                                               ; down
+      37 (jump-to-frame! (max 0                             ; left
+                              (if (.-shiftKey e)
+                                nearest-tick-left
+                                (dec now))))
+      39 (jump-to-frame! (min (dec duration)                ; right
+                              (if (.-shiftKey e)
+                                nearest-tick-right
+                                (inc now))))
+      true)
+    (.preventDefault e)))
 (.addEventListener js/document "keydown" handle-keyboard!)
 
 (defn tick-mark [data _owner {w :width}]
@@ -132,25 +160,24 @@
                                  :width          "0px"
                                  :height         "96%"
                                  :position       :absolute
-                                 :left           (+ 6 (* (/ w (:context data)) (:frame data)))
+                                 :left           (+ 6 (frame-offset-x (:frame data) w (:context data)))
                                  :top            0
                                  :pointer-events "none"}})))))
 
-
-(defn shown-tick-frames [w context left duration]
-  (let [left-frame (inv-frame-offset-x left w context duration)]
-    (range left-frame (min (+ left-frame context) duration) 16)))
-
 (defn timeline [data owner {w :width h :height y :y}]
   (reify
-    om/IInitState
-    (init-state [_] {:scroll-x 0})
     om/IDidMount
     (did-mount [_]
-      (om/set-state! owner {:scroll-x (.-scrollLeft (om/get-node owner))}))
-    om/IRenderState
-    (render-state [_ {scroll-x :scroll-x}]
+      (om/transact! (:timeline data)
+                    [:scroll-x]
+                    (fn [_] (.-scrollLeft (om/get-node owner)))))
+    om/IDidUpdate
+    (did-update [_ _ _]
+      (set! (.-scrollLeft (om/get-node owner)) (get-in data [:timeline :scroll-x])))
+    om/IRender
+    (render [_]
       (let [context (:context (:timeline data))
+            scroll-x (:scroll-x (:timeline data))
             now (:now data)
             duration (:duration data)]
         (dom/div
@@ -161,7 +188,9 @@
                                :width      (str w "px")
                                :height     (str h "px")}
                     :onScroll (fn [_e]
-                                (om/set-state! owner {:scroll-x (.-scrollLeft (om/get-node owner))}))})
+                                (om/transact! (:timeline data)
+                                              [:scroll-x]
+                                              (fn [_] (.-scrollLeft (om/get-node owner)))))})
           (dom/div (clj->js {:style   {:backgroundColor     "rgb(50,50,200)"
                                        :border              "16px solid green"
                                        :border-left-width   "8px"
@@ -193,14 +222,15 @@
                                      :left           (+ 1 (frame-offset-x now w context))
                                      :top            0}})))))))
 
-(def app-state (atom {:source      (frame-image-provider)
-                      :metadata    {}
-                      :now         0
-                      :timeline    {:context  1000
-                                    :scroll-x 0}
-                      :annotations []
-                      :edits       []
-                      :duration    2000}))
+(def app-state (atom {:source       (frame-image-provider)
+                      :metadata     {}
+                      :now          0
+                      :timeline     {:context  1000
+                                     :scroll-x 0}
+                      :scroll-width 800
+                      :annotations  []
+                      :edits        []
+                      :duration     2000}))
 
 (om/root
   (fn [data _owner]
@@ -213,7 +243,7 @@
                             :now      (:now data)
                             :edits    (:edits data)
                             :duration (:duration data)}
-                           {:opts {:width 800 :height 100 :y 478}})))))
+                           {:opts {:width (:scroll-width data) :height 100 :y 478}})))))
   app-state
   {:target (.getElementById js/document "app")})
 
